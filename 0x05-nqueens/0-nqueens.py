9d982f4cb0158c1369e02f046cf8d33951f18b6f
Kevin  Ngugi <kevinngugi197@gmail.com>
2023-11-02 14:56:07 +0300

    changes to n-queen

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -2,38 +2,92 @@
 
 import sys
 
-if len(sys.argv) != 2:
-  print("Usage: nqueens N")
-  exit(1)
+if __name__ == '__main__':
 
-if not sys.argv[1].isdigit():
-  print("N must be a number")
-  exit(1)
+  if len(sys.argv) != 2:
+    print("Usage: nqueens N")
+    sys.exit(1)
 
-n = int(sys.argv[1])  
+  try:
+    n = int(sys.argv[1])
+  except ValueError:
+    print('N must be a number')
+    exit(1)
 
-if n < 4:
-  print("N must be at least 4")
-  exit(1)
-
-def queens(n, row=0, cols=[], diag1=[], diag2=[]):
-
-  if row == n:
-    yield cols
-
-  for col in range(n):
-    if col not in cols and row+col not in diag1 and row-col not in diag2:
-      yield from queens(n, row+1, cols+[col], diag1+[row+col], diag2+[row-col])
-
-
-def solve(n):
+  if n < 4:
+    print('N must be at least 4')
+    exit(1)
 
   solutions = []
-  for solution in queens(n):
-    solutions.append(solution)
-  
-  for solution in solutions:
-    print([[i, col] for i, col in enumerate(solution)])
+  placed_queens = []
 
-if __name__ == "__main__":
-  solve(n)
+  stop = False
+  r = 0
+  c = 0
+
+  while r < n and not stop:
+
+    goback = False
+
+    while c < n:
+
+      safe = True
+
+      for cord in placed_queens:
+        col = cord[1]
+        
+        if (col == c or 
+            col + (r-cord[0]) == c or
+            col - (r-cord[0]) == c):
+          
+          safe = False
+          break
+
+      if not safe:
+        if c == n - 1:
+          goback = True
+          break
+
+        c += 1
+        continue
+
+      cords = [r, c]
+      placed_queens.append(cords)
+
+      if r == n - 1:
+        solutions.append(placed_queens[:])
+        
+        for cord in placed_queens:
+          if cord[1] < n - 1:
+            r = cord[0]
+            c = cord[1]
+            for i in range(n - r):
+              placed_queens.pop()
+          
+          if r == n - 1 and c == n - 1:
+            placed_queens = []
+            stop = True
+
+        r -= 1
+        c += 1
+
+      else:
+        c = 0
+        break
+
+    if goback:
+      r -= 1
+      while r >= 0:
+        c = placed_queens[r][1] + 1
+        del placed_queens[r]
+        if c < n:
+          break
+        r -= 1
+
+      if r < 0:
+        break
+
+    r += 1
+
+  for solution in solutions:
+    print(solution)

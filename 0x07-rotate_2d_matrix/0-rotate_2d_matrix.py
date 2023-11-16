b03ac148e14701e7d8b20092ef66721c08708b18
Kevin  Ngugi <kevinngugi197@gmail.com>
2023-11-16 22:13:54 +0300

    updated

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,17 +1,10 @@
 #!/usr/bin/python3
-
-
 def rotate_2d_matrix(matrix):
-    '''scripts to rotate nxn 2D matrix clockwise
-    Return: Nothing '''
-    
-  # step-1: transpose the matrix
-  for i in range(len(matrix)):
-    for j in range(i, len(matrix)):
-      matrix[i][j],matrix[j][i]=matrix[j][i],matrix[i][j]
-
-  # step-2: reverse the matrix
-  N=len(matrix)
-  for i in range(N//2):
-    for j in range(N):
-      matrix[j][i],matrix[j][N-1-i]=matrix[j][N-1-i],matrix[j][i]
+    n = len(matrix)
+    for i in range(n // 2):
+        for j in range(i, n - i - 1):
+            temp = matrix[i][j]
+            matrix[i][j] = matrix[n - 1 - j][i]
+            matrix[n - 1 - j][i] = matrix[n - 1 - i][n - 1 - j]
+            matrix[n - 1 - i][n - 1 - j] = matrix[j][n - 1 - i]
+            matrix[j][n - 1 - i] = temp
--- 0x07-rotate_2d_matrix/main_0.py
+++ 0x07-rotate_2d_matrix/main_0.py
@@ -10,4 +10,4 @@ if __name__ == "__main__":
               [7, 8, 9]]
 
     rotate_2d_matrix(matrix)
-    print(matrix
+    print(matrix)
